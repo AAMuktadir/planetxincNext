1c8fbecb90a75ce4c4263383cda8eaa32abf212c
AAMuktadir <md.abdullah.al.muktadir@g.bracu.ac.bd>
2025-04-26 23:32:18 +0600

    test update

--- Components/home/testAnimation.jsx
+++ Components/home/testAnimation.jsx
@@ -1,11 +1,22 @@
 "use client";
 import React from "react";
 import Spline from "@splinetool/react-spline";
+import Link from "next/link";
 
 export default function TestAnimation() {
   return (
-    <div className="w-full h-screen">
+    <div className="relative w-full h-screen">
       <Spline scene="https://prod.spline.design/J-29532UxVlnfs-Q/scene.splinecode" />
+
+      <div className=" absolute bottom-12 flex items-center justify-center text-white w-full">
+        <Link
+          href={"https://atnation.live/"}
+          target="_blank"
+          className="text-white uppercase text-xl font-medium hover:text-footer duration-300 hover:underline"
+        >
+          click here
+        </Link>
+      </div>
     </div>
   );
 }
